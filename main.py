34333dd25a97e20d5261e93d5d077371931fb041
Ilya Kotsar <ilyakotsar@proton.me>
2022-11-09 14:32:25 +0300

    Update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    text = 'Enter your username to enable notifications.'
+    text = 'Enter your username to enable notifications'
     bot.send_message(message.chat.id, text)
 
 
@@ -21,13 +21,13 @@ def echo_all(message):
     con = sqlite3.connect(DATABASE_PATH)
     cur = con.cursor()
     username = message.text.strip()
-    query1 = f"SELECT username_lower FROM main_user WHERE username_lower='{username.lower()}'"
+    query1 = f"SELECT username FROM main_user WHERE username='{username}'"
     res = cur.execute(query1)
     fetch1 = res.fetchone()
     if fetch1 is None:
         output = 'Error'
     else:
-        query2 = f"SELECT telegram_connected FROM main_user WHERE username_lower='{username.lower()}'"
+        query2 = f"SELECT telegram_connected FROM main_user WHERE username='{username}'"
         res = cur.execute(query2)
         fetch2 = res.fetchone()
         if fetch2[0] == 1:
@@ -36,7 +36,7 @@ def echo_all(message):
             while True:
                 link = uuid.uuid4()
                 h = hashlib.sha256()
-                h.update(link.encode())
+                h.update(str(link).encode())
                 link_hash = h.hexdigest()
                 query3 = f"SELECT telegram_link_hash FROM main_user WHERE telegram_link_hash='{link_hash}'"
                 res = cur.execute(query3)
@@ -46,12 +46,12 @@ def echo_all(message):
                 else:
                     continue
             chat_id = message.chat.id
-            query4 = f"UPDATE main_user SET telegram_chat_id='{chat_id}' WHERE username_lower='{username.lower()}'"
-            query5 = f"UPDATE main_user SET telegram_link_hash='{link_hash}' WHERE username_lower='{username.lower()}'"
+            query4 = f"UPDATE main_user SET telegram_chat_id='{chat_id}' WHERE username='{username}'"
+            query5 = f"UPDATE main_user SET telegram_link_hash='{link_hash}' WHERE username='{username}'"
             cur.execute(query4)
             cur.execute(query5)
             con.commit()
-            output = f'Open link https://messedges.com/tg/{link} as user {username} to enable notifications.'
+            output = f'Open link https://messedges.com/tg/{link} as user {username} to enable notifications'
     bot.send_message(message.chat.id, output)
 
 
